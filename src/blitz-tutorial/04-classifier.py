caeed657b0fd120847be3792c378b3f0cbe94c84
coreycole <coreyleoc@gmail.com>
2019-03-05 12:34:31 -0800

    feat(blitz-tutorial): finish classifier with notes for using GPU for training performance

--- src/blitz-tutorial/04-classifier.py
+++ src/blitz-tutorial/04-classifier.py
@@ -9,6 +9,9 @@ Training an image classifier
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -40,10 +43,6 @@ classes = (
     'dog', 'frog', 'horse', 'ship', 'truck'
 )
 
-"""
-Showing images
-"""
-
 
 def img_show(img):
     """Show an image"""
@@ -62,4 +61,147 @@ print('showing an image with...')
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 # show images
-img_show(torchvision.utils.make_grid(images))
+# img_show(torchvision.utils.make_grid(images))
+
+"""
+Define a Convolutional Neural Network
+"""
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)  # 3 in_channels for RGB images
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+"""
+Define a Loss function and optimizer
+"""
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+"""
+Train the network
+"""
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+"""
+Test the network on the test data
+"""
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+# img_show(torchvision.utils.make_grid(images))
+print('GroundTruth: ',
+      ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+outputs = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ',
+      ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+"""
+Check accuracy of net on the whole dataset
+"""
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+      100 * correct / total))
+
+"""
+Check accuracy of net on a class-by-class basis
+"""
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+"""
+Check for GPU device
+"""
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+"""
+Transfer work to GPU if you have one
+"""
+net.to(device)
+
+"""
+Transfer inputs and targets to GPU
+"""
+inputs, labels = inputs.to(device), labels.to(device)
+
+"""
+Why don't we notice a massive speedup compared to CPU here?
+- The current neural network is very small
+- Try increasing the width of the network
+  - argument 2 of the first nn.Conv2d (out channels)
+  - argument 1 of the second nn.Conv2d (in channels, need to be the same number)
+"""
